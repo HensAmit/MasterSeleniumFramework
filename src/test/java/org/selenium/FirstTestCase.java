67e5915133ff3fbfc94fdfdc69fccf5f15b8c9ac
Amit Hens <hens.is.here@gmail.com>
2022-01-14 15:57:06 +0530

    Home page, store page added

--- src/test/java/org/selenium/FirstTestCase.java
+++ src/test/java/org/selenium/FirstTestCase.java
@@ -1,12 +1,20 @@
 package org.selenium;
 
 import org.selenium.pom.base.BaseTest;
+import org.selenium.pom.pages.HomePage;
+import org.selenium.pom.pages.StorePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstTestCase extends BaseTest {
     @Test
-    public void demoTest(){
+    public void guestCheckOutUsingDirectBankTransfer(){
         driver.get("https://askomdch.com/");
-        driver.manage().window().maximize();
+        HomePage homePage = new HomePage(driver);
+        StorePage storePage = homePage.clickStoreMenuLink();
+        storePage.enterTextInSearchInputField("Blue");
+        storePage.clickSearchBtn();
+        Assert.assertEquals(storePage.getTitle(), "Search results: “Blue”");
+        storePage.clickAddToCartBtn();
     }
 }
---
+++ src/test/java/org/selenium/pom/pages/HomePage.java
@@ -0,0 +1,18 @@
+package org.selenium.pom.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.selenium.pom.base.BasePage;
+
+public class HomePage extends BasePage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    private final By storeMenuLink = By.cssSelector("[id='menu-item-1227']>a");
+
+    public StorePage clickStoreMenuLink(){
+        driver.findElement(storeMenuLink).click();
+        return new StorePage(driver);
+    }
+}
