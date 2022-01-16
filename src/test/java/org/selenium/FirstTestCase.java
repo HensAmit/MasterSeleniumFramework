c00e637db4259254bab3ee2b880666448879d2aa
Amit Hens <hens.is.here@gmail.com>
2022-01-16 15:37:35 +0530

    Added implicit wait
    Removed all the static sleep commands

--- src/test/java/org/selenium/FirstTestCase.java
+++ src/test/java/org/selenium/FirstTestCase.java
@@ -16,19 +16,16 @@ import java.io.IOException;
 
 public class FirstTestCase extends BaseTest {
     @Test
-    public void guestCheckOutUsingDirectBankTransfer() throws InterruptedException, IOException {
+    public void guestCheckOutUsingDirectBankTransfer() throws IOException {
         HomePage homePage = new HomePage(driver).loadURL();
         StorePage storePage = homePage.navigateToStoreUsingMenu();
-        Thread.sleep(10000);
 
         storePage.enterTextInSearchInputField("Blue");
         storePage.clickSearchBtn();
-        Thread.sleep(5000);
         Assert.assertEquals(storePage.getTitle(), "Search results: “Blue”");
 
         Product product = new Product(1215);
         storePage.clickAddToCartBtn(product.getName());
-        Thread.sleep(10000);
 
         CartPage cartPage = storePage.clickViewCartLink();
         Assert.assertEquals(cartPage.getProductName(), product.getName());
@@ -38,7 +35,6 @@ public class FirstTestCase extends BaseTest {
         checkOutPage
                 .setBillingAddress(billingAddress)
                 .placeOrder();
-        Thread.sleep(15000);
         Assert.assertEquals(checkOutPage.getSuccessNotice(), "Thank you. Your order has been received.");
     }
 
@@ -46,22 +42,18 @@ public class FirstTestCase extends BaseTest {
     public void logInAndCheckOutUsingDirectBankTransfer() throws InterruptedException, IOException {
         HomePage homePage = new HomePage(driver).loadURL();
         StorePage storePage = homePage.navigateToStoreUsingMenu();
-        Thread.sleep(10000);
 
         storePage.enterTextInSearchInputField("Blue");
         storePage.clickSearchBtn();
-        Thread.sleep(5000);
         Assert.assertEquals(storePage.getTitle(), "Search results: “Blue”");
 
         Product product = new Product(1215);
         storePage.clickAddToCartBtn(product.getName());
-        Thread.sleep(5000);
 
         CartPage cartPage = storePage.clickViewCartLink();
         Assert.assertEquals(cartPage.getProductName(), product.getName());
         CheckOutPage checkOutPage = cartPage.checkOut();
         checkOutPage.clickHereToLoginLink();
-        Thread.sleep(3000);
 
         BillingAddress billingAddress = JacksonUtils.deserializeJson("src\\test\\resources\\myBillingAddress.json", BillingAddress.class);
         User user = new User("hens1", "demopassword");
@@ -69,7 +61,6 @@ public class FirstTestCase extends BaseTest {
                 .login(user)
                 .setBillingAddress(billingAddress)
                 .placeOrder();
-        Thread.sleep(10000);
         Assert.assertEquals(checkOutPage.getSuccessNotice(), "Thank you. Your order has been received.");
     }
 }
