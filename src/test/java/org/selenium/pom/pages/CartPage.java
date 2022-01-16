42e9af14c0392231cb58f7783d49d1188605d0f9
Amit Hens <hens.is.here@gmail.com>
2022-01-16 22:27:21 +0530

    PageFactory implemented in CartPage

--- src/test/java/org/selenium/pom/pages/CartPage.java
+++ src/test/java/org/selenium/pom/pages/CartPage.java
@@ -2,25 +2,35 @@ package org.selenium.pom.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.selenium.pom.base.BasePage;
 
 public class CartPage extends BasePage {
     public CartPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
-    private final By productName = By.cssSelector("td[class='product-name'] > a");
-    private final By checkOutBtn = By.cssSelector(".checkout-button");
+//    private final By productName = By.cssSelector("td[class='product-name'] > a");
+//    private final By checkOutBtn = By.cssSelector(".checkout-button");
+
+    @FindBy(css = "td[class='product-name'] > a")
+    private WebElement productName;
+
+    @FindBy(css = ".checkout-button")
+    private WebElement checkOutBtn;
 
     public String getProductName(){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(productName));
-        return driver.findElement(productName).getText();
+        wait.until(ExpectedConditions.visibilityOf(productName));
+        return productName.getText();
     }
 
     public CheckOutPage checkOut(){
         wait.until(ExpectedConditions.elementToBeClickable(checkOutBtn));
-        driver.findElement(checkOutBtn).click();
+        checkOutBtn.click();
         return new CheckOutPage(driver);
     }
 }
