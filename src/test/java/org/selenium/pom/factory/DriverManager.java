84b41a15f104fdfeb14f103bfecc0dec63736e4d
Amit Hens <hens.is.here@gmail.com>
2022-01-17 17:13:02 +0530

    Multiple browser support through mvn

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -3,12 +3,25 @@ package org.selenium.pom.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
 
     public WebDriver initializeDriver(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+        String browser = System.getProperty("browser");
+        switch (browser.toLowerCase()){
+            case "chrome" :
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox" :
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default :
+                throw new IllegalArgumentException("INVALID BROWSER NAME: " + browser);
+        }
         driver.manage().window().maximize();
         return driver;
     }
